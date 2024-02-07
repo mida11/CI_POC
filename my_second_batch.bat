40837c305898d35c270c5b6d28f37ad7a5eaf118
MohamedAli <56230767+mida11@users.noreply.github.com>
2024-02-07 04:44:28 -0800

    Update my_second_batch.bat

--- my_second_batch.bat
+++ my_second_batch.bat
@@ -1,4 +1,4 @@
 @echo off
 title My second batch file
-echo Congratulations and welcome to your second batch file! heloooo
+echo Congratulations and welcome to your second batch file! hellllllloooo
 pause
